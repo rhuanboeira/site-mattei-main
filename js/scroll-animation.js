09f760d80757541f4bdb6a9314daf97ba02d74f5
Rhuan Boeira <rhuannboeira@gmail.com>
2025-05-06 16:30:12 -0300

    Scroll animation

---
+++ js/scroll-animation.js
@@ -0,0 +1,65 @@
+window.sr = ScrollReveal({ reset: true }); 
+
+sr.reveal('.overlay-text', {
+  origin: 'left', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 100, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('.overlay', {
+  origin: 'left', 
+  distance: '5%', 
+  duration: 1500, 
+  delay: 100, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('#sobre', {
+  origin: 'top', 
+  distance: '20%', 
+  duration: 2000, 
+  delay: 200, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('#loja', {
+  origin: 'right', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 200, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('#produtos', {
+  origin: 'bottom', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 150, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('.feedback', {
+  origin: 'right', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 150, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('.endereco', {
+  origin: 'bottom', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 150, 
+  easing: 'ease-in-out' 
+});
+
+sr.reveal('.contato-conteudo', {
+  origin: 'bottom', 
+  distance: '20%', 
+  duration: 1000, 
+  delay: 200, 
+  easing: 'ease-in-out' 
+});
